7e9d14c9b1752fb565683d6e6694f23822448964
Richard Hrnciarik <richard.hrnciarik@gmail.com>
2025-10-04 21:24:52 +0200

    Add files via upload

---
+++ 157-17.py
@@ -0,0 +1,14 @@
+import tkinter
+from random import *
+canvas = tkinter.Canvas(height = 600, width = 1200, bg = 'purple')
+canvas.pack()
+
+vstup_x = open('x.txt', 'r')
+vstup_y = open('y.txt', 'r')
+
+for riadok in vstup_x:
+    x = vstup_x.readline()
+    y = vstup_y.readline()
+    canvas.create_oval(int(x) - 5, int(y) -5, int(x) + 5, int(y) + 5, outline = 'white')
+
+canvas.mainloop()
